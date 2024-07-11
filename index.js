c4f783bc5c646f040b9b5f77d38bf9ca3d2540e1
Ehsanullah946 <Ehsanullahakbari53@gmail.com>
2024-07-10 19:14:07 -0700

    change style

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function convert(){
+    
     var birthYear=document.getElementById('year').value;
     var birthMonth=document.getElementById('month').value;
     var birthDay=document.getElementById('day').value;
@@ -6,12 +7,12 @@ function convert(){
       var currentMonth=new Date().getMonth()+1;
       var currentDay=new Date().getDay();
       const age=currentYear - birthYear;
-      const month=(age*12) + currentMonth - birthMonth;
-      const month2=Math.abs((age*12)-month); 
-      const day= (age*365) + (month2*30) + currentDay-birthDay;
-      const day2=Math.abs((day - (age*365) + (month2*30)-365));
-      // const day2 = day-(age*365);
+      const allMonth=(age*12) + currentMonth - birthMonth;
+      const month=Math.abs((age*12)-allMonth); 
+      const allDay= (age*365) + (month2*30) + currentDay-birthDay;
+      const day=Math.abs((allDay - (age*365) + (month2*30)-365));
       document.getElementById('show1').innerHTML=age;
-      document.getElementById('show2').innerHTML=month2;
-      document.getElementById('show3').innerHTML=day2;
+      document.getElementById('show2').innerHTML=month;
+      document.getElementById('show3').innerHTML=day;
     }
+   
