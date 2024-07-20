7a7167c57fac3d988b1d36c2dbe928e66f3ad90d
Ehsanullah946 <Ehsanullahakbari53@gmail.com>
2024-07-20 09:17:44 -0700

    solved problem

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function convert(){
       const age=currentYear - birthYear;
       const allMonth=(age*12) + currentMonth - birthMonth;
       const month=Math.abs((age*12)-allMonth); 
-      const allDay= (age*365) + (month2*30) + currentDay-birthDay;
-      const day=Math.abs((allDay - (age*365) + (month2*30)-365));
+      const allDay= (age*365) + (month*30) + currentDay-birthDay;
+      const day=Math.abs((allDay -((age*365) + (month*30))));
       document.getElementById('show1').innerHTML=age;
       document.getElementById('show2').innerHTML=month;
       document.getElementById('show3').innerHTML=day;
